d29f706a4f0371c09f5b9bde1404573d22cb0da5
Sarvesh Warge <sarveshwarge@gmail.com>
2020-04-19 10:38:00 +0530

    made script compatible with non-heroku environments

--- start.sh
+++ start.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-# required for n8n to start on heroku alloted port
-export N8N_PORT=$PORT
+# check if port variable is set or go with default
+if [ -z ${PORT+x} ]; then echo "PORT variable not defined, leaving N8N to default port."; else export N8N_PORT=$PORT; echo "N8N will start on '$PORT'"; fi
 
 # kickstart nodemation
 n8n
